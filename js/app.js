33093107587b13771d514416cc8d29c7b3d61954
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-29 12:22:38 +0100

    Added logic to toggle chevron

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
 	function toggle(chevron) {
-
+		if (Array.from(chevron.classList).indexOf("fa-chevron-up") === -1) {
+			chevron.classList.remove("fa-chevron-down");
+			chevron.classList.add("fa-chevron-up");
+		} else {
+			chevron.classList.remove("fa-chevron-up");
+			chevron.classList.add("fa-chevron-down");
+		}
 	}
 
 	let chevron = document.getElementsByClassName("fas")[0];

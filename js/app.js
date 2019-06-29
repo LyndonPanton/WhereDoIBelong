c73883410703c23a95805104e5c6a518fb783f67
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-29 12:26:11 +0100

    Added logic to toggle task details

--- js/app.js
+++ js/app.js
@@ -4,12 +4,20 @@ window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
 	function toggle(chevron) {
+		let task = document.getElementById("task");
+
 		if (Array.from(chevron.classList).indexOf("fa-chevron-up") === -1) {
 			chevron.classList.remove("fa-chevron-down");
 			chevron.classList.add("fa-chevron-up");
+
+			task.classList.remove("hidden");
+			task.classList.add("visible");
 		} else {
 			chevron.classList.remove("fa-chevron-up");
 			chevron.classList.add("fa-chevron-down");
+
+			task.classList.remove("visible");
+			task.classList.add("hidden");
 		}
 	}
 
